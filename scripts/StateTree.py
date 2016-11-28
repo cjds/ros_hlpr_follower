09a85014af445569e52ddb99852e3c489b8467e9
Carl Saldanha <cjds@live.com>
2016-11-27 21:25:14 -0500

    added test functions

--- scripts/StateTree.py
+++ scripts/StateTree.py
@@ -10,6 +10,17 @@ from std_msgs.msg import String
 from geometry_msgs.msg import Twist,Pose,PoseStamped
 from sensor_msgs.msg import Image
 
+
+class Node:
+	def __init__(self,p,q):
+		self.p=Information()
+  		self.q=Information()
+  		self.children=[]
+
+  	def addChild(self,node):
+  		self.children.append(node)
+
+
 class StateTree:
 
   '''
@@ -18,27 +29,55 @@ class StateTree:
   def __init__(self, collision_checker):
 
   	self.collision_check=collision_checker
-
-  	self.bmax=3
+ 
+ 	self.bmax=3
   	self.dmax=2
 
   	self.p=Information()
   	self.q=Information()
 
+  	self.root=Node()
+
+  	self.Q= PriorityQueue()
+
+  def getAvailableActions(q):
+  	pass
+
+  def plan(self,q):
+  	current_node=[]
+
+  	self.Q.put(q)
+
+  	while True:
+  		if self.Q.empty():
+  			return self.backtrace(qbest)
+  		current_node.append(Q.get())
+  		L=[]
+  		for action in self.getAvailableActions(q):
+  			L.append(action)
+
+		L=(sorted(L, key=lambda x: x.modified, reverse=True))  	
   '''
   	Update this tree 
   '''
-  def updateTree():
+  def updateTree(self):
   	pass
 
-  def resolveTree():
+  '''
+  	Update the values in the tree by recreating the root
+  '''
+  def resolveTree(self):
   	pass
 
-  def goal(p,q):
+  def goal(self,p,q):
   	return 0
 
-  def utility():
+  def utility(self,node):
   	return 0
+
+  def getNodes(sel.f):
+  	nodes=[]
+  	for
 
 
 
--- scripts/test.py
+++ scripts/test.py
@@ -1,1 +1,25 @@
-te
+'''
+This test file takes state information and displays it
+'''
+
+error=0
+StateTree= StateTree()
+
+#new las
+def new_laser_scan_data(data):
+
+	#2d pose estimate
+
+	#figure out best velocity
+
+	#send out new velocity command
+	pass
+
+def new_odometry(data):
+	pass
+
+def new_robot_information_data(data):
+	pass
+
+def predict_next_move():
+	pass
