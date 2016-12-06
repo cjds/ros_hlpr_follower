5a20acfa346b98db591598aa3e528cdda0386001
Carl Saldanha <cjds@live.com>
2016-12-06 03:15:21 -0500

    added playpen test points

--- nodes/follower_node.py
+++ nodes/follower_node.py
@@ -18,9 +18,39 @@ class follower_node:
 
   def __init__(self):
     #publisher
-    #self.teleop_pub = rospy.Publisher("/vector/teleop/cmd_vel",Twist)
-    #self.move_base_goal =rospy.Publisher("move_base_navi/goal",MoveBaseActionGoal,queue_size=10)
 
+    people_positions=[]
+    people_positions.append([8.339, 4.507, 0.000, 0.000, 0.000, -0.766, 0.643])
+    people_positions.append([7.224, 1.832, 0.000, 0.000, 0.000, -0.850, 0.527])
+    people_positions.append([6.694, 0.998, 0.000,0.000, 0.000, 0.887, -0.462])
+    people_positions.append([6.765, 1.050, 0.000,0.000, 0.000, -0.825, 0.565])
+    people_positions.append([5.716, -1.611, 0.000,0.000, 0.000, -0.516, 0.857])
+    people_positions.append([4.744, -2.189, 0.000,0.000, 0.000, 0.999, 0.049])
+    people_positions.append([2.052, -0.760, 0.000,0.000, 0.000, 0.940, 0.341])
+    people_positions.append([0.454, 0.798, 0.000,0.000, 0.000, 0.930, 0.368])
+    people_positions.append([-1.421, 2.756, 0.000,0.000, 0.000, 0.834, 0.552])
+    people_positions.append([-3.082, 4.578, 0.000,0.000, 0.000, 0.958, 0.288])
+
+    #people_positions.append([2.614, -2.580, 0.000, 0.000, 0.000, -0.703, 0.711])
+    #people_positions.append([2.423, -5.094, 0.000,0.000, 0.000, 0.961, -0.278])
+    #people_positions.append([2.365, -6.400, 0.000,0.000, 0.000, -0.799, 0.602])
+    #people_positions.append([1.848, -7.329, 0.000,0.000, 0.000, 0.964, -0.264])
+    #people_positions.append([0.689, -7.275, 0.000,0.000, 0.000, 0.885, 0.465])
+    #people_positions.append([0.685, -6.313, 0.000, 0.000, 0.000, 0.566, 0.824]) 
+    #people_positions.append([2.153, -5.434, 0.000,0.000, 0.000, 0.364, 0.931])
+    #people_positions.append([3.162, -4.180, 0.000,0.000, 0.000, 0.364, 0.931]) 
+    #people_positions.append([3.162, -4.180, 0.000, 0.000, 0.000, 0.502, 0.865])
+    #people_positions.append([4.689, -2.513, 0.00, 0.000, 0.000, 0.445, 0.896])
+    #people_positions.append([4.775, -0.466, 0.000,0.000, 0.000, 0.772, 0.636])
+    #people_positions.append([4.775, -0.466, 0.000,0.000, 0.000, 0.772, 0.636])
+    #people_positions.append([3.180, 0.721, 0.000,0.000, 0.000, 0.926, 0.377])
+    #people_positions.append([1.886, 1.627, 0.00,0.000, 0.000, 0.984, 0.176])
+    #people_positions.append([-0.478, 1.025, 0.000,0.000, 0.000, 0.991, -0.131])
+    #people_positions.append([0.446, -0.392, 0.000,0.000, 0.000, -0.489, 0.872])
+
+    velocity=19.0
+
+    size=velocity*(len(people_positions)-1)
     #frames
     self.radius= 1.0 #radius at which its considered acceptable to move
     #subscribers
@@ -29,48 +59,51 @@ class follower_node:
     self.visualization_marker_pub = rospy.Publisher("/person_display",Marker,queue_size=10)
 
     self.tf = tf.TransformListener()
-    size=10
     positions=[]
     self.client = actionlib.SimpleActionClient('move_base_navi', MoveBaseAction)
     self.client.wait_for_server()
     
     #positions and orientations representing poses of the human
-    for i in range(size):
-      positions.append(Point())
-      positions[i].x=0.5 + (i*0.5)
-      positions[i].y=0.5 + (i*0.5)
-      positions[i].z=0.0
+    for i in range(len(people_positions)-1):
+      for  j in range(int(velocity)):
+        c=int(i*len(people_positions) + j)
+        positions.append(Point())
+        positions[c].x=people_positions[i][0]+((people_positions[i][0]-people_positions[i+1][0]) * j / velocity)
+        positions[c].y=people_positions[i][1]+((people_positions[i][1]-people_positions[i+1][1]) * j / velocity)
+        positions[c].z=people_positions[i][2]+((people_positions[i][2]-people_positions[i+1][2]) * j /  velocity)
     orientation=[]
-    for i in range(size):
-      orientation.append(Quaternion())
-      orientation[i].w=0.5 + (i*0.5)
-      orientation[i].z=0.5 + (i*0.5)
-      orientation[i].y=0.0
-      orientation[i].x=0.0
+    for i in range(len(people_positions)-1):
+      for  j in range(int(velocity)):
+        c=int(i*len(people_positions) + j)
+        orientation.append(Quaternion())
+        orientation[c].x=people_positions[i][3]+((people_positions[i][3]-people_positions[i+1][3]) * j / velocity)
+        orientation[c].y=people_positions[i][4]+((people_positions[i][4]-people_positions[i+1][4]) * j / velocity)
+        orientation[c].z=people_positions[i][5]+((people_positions[i][5]-people_positions[i+1][5]) * j / velocity)
+        orientation[c].w=people_positions[i][6]+((people_positions[i][6]-people_positions[i+1][6]) * j / velocity)
 
 
     r = rospy.Rate(5) # 10hz
     count=0
     while not rospy.is_shutdown():
-      new_pose=PoseStamped()
-      new_pose.header.frame_id='/map'
-      new_pose.header.stamp=rospy.Time.now()
-      new_pose.pose.position=positions[count%size]
-      new_pose.pose.orientation=orientation[count%size]
-      count+=1
-      self.image_teleop_pub.publish(new_pose)
-      visualization_marker=Marker()
-      visualization_marker.header.frame_id='/map'
-      visualization_marker.header.stamp=rospy.Time.now()  
-      visualization_marker.type=1
-      visualization_marker.scale.x=1.0
-      visualization_marker.scale.y=1.0
-      visualization_marker.scale.z=1.0
-      visualization_marker.color.r=1.0
-      visualization_marker.color.a=1.0
-      visualization_marker.pose=new_pose.pose
-      visualization_marker.lifetime=rospy.Duration(5.0)
-      self.visualization_marker_pub.publish(visualization_marker)
+      #new_pose=PoseStamped()
+      #new_pose.header.frame_id='/map'
+      #new_pose.header.stamp=rospy.Time.now()
+      #new_pose.pose.position=positions[int(count%size)]
+      #new_pose.pose.orientation=orientation[int(count%size)]
+      #count+=1
+      #self.image_teleop_pub.publish(new_pose)
+      #visualization_marker=Marker()
+      #visualization_marker.header.frame_id='/map'
+      #visualization_marker.header.stamp=rospy.Time.now()  
+      #visualization_marker.type=1
+      #visualization_marker.scale.x=1.0
+      #visualization_marker.scale.y=1.0
+      #visualization_marker.scale.z=1.0
+      #visualization_marker.color.r=1.0
+      #visualization_marker.color.a=1.0
+      #visualization_marker.pose=new_pose.pose
+      #visualization_marker.lifetime=rospy.Duration(5.0)
+      #self.visualization_marker_pub.publish(visualization_marker)
       r.sleep()
 
 
